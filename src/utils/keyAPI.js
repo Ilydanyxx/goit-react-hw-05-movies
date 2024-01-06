6945f162fbfaaab9ce7218690d6089316ef2b80a
Ilydanyxx <danya.chuyko@icloud.com>
2024-01-07 00:18:00 +0200

    update

---
+++ src/utils/keyAPI.js
@@ -0,0 +1,5 @@
+export const BASE_URL = 'https://api.themoviedb.org/3/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w300/';
+export const IMG_URL_CAST = 'https://image.tmdb.org/t/p/w200/';
+export const API_KEY = '536566c9dd222e8505bf9bc3c6757305';
+
